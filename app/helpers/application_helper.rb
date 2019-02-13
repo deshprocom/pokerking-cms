5deee77cd8ea99464df78fac5688fd387d608fb3
breeze-yang <breeze@deshpro.com>
2019-02-13 16:30:43 +0800

    修改config 数据库中的时区设置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def img_link_to_show(resource, preview)
-    link_to image_tag(preview, height: 150), resource_path(resource)
+    link_to image_tag(preview.to_s, height: 150), resource_path(resource)
   end
 
   def main_event_sidebar_generator(context)
