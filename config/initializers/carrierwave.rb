801c36ea1446cb775bf40e72f59aa28ec6a381b8
breeze-yang <breeze@deshpro.com>
2019-03-08 15:17:30 +0800

    完善carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,16 @@
-# 暂时不需要cdn
+# 生产环境暂时没有cdn
 CarrierWave.configure do |config|
-  config.storage = :file
-  config.base_path = ENV['ASSET_HOST']
-  config.remove_previously_stored_files_after_update = false
+  if ENV['ASSET_HOST'].present?
+    config.storage = :file
+    config.base_path = ENV['ASSET_HOST']
+    config.remove_previously_stored_files_after_update = false
+  else
+    config.storage = :upyun
+    config.upyun_username    = ENV['UPYUN_USERNAME']
+    config.upyun_password    = ENV['UPYUN_PASSWD']
+    config.upyun_bucket      = ENV['UPYUN_BUCKET']
+    config.upyun_bucket_host = ENV['UPYUN_BUCKET_HOST']
+  end
   # if Rails.env.test?
   #   config.storage = :file
   # else
