9f533e2950b39251c27ed1b78bd63cc5734ee26a
breeze-yang <breeze@deshpro.com>
2019-03-08 11:34:59 +0800

    更新操作时不删除文件

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
 # 暂时不需要cdn
-# CarrierWave.configure do |config|
-#   if Rails.env.test?
-#     config.storage = :file
-#   else
-#     config.storage = :upyun
-#     config.upyun_username    = ENV['UPYUN_USERNAME']
-#     config.upyun_password    = ENV['UPYUN_PASSWD']
-#     config.upyun_bucket      = ENV['UPYUN_BUCKET']
-#     config.upyun_bucket_host = ENV['UPYUN_BUCKET_HOST']
-#   end
-# end
+CarrierWave.configure do |config|
+  config.storage = :file
+  config.base_path = ENV['ASSET_HOST']
+  config.remove_previously_stored_files_after_update = false
+  # if Rails.env.test?
+  #   config.storage = :file
+  # else
+  #   config.storage = :upyun
+  #   config.upyun_username    = ENV['UPYUN_USERNAME']
+  #   config.upyun_password    = ENV['UPYUN_PASSWD']
+  #   config.upyun_bucket      = ENV['UPYUN_BUCKET']
+  #   config.upyun_bucket_host = ENV['UPYUN_BUCKET_HOST']
+  # end
+end
