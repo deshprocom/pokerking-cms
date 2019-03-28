f6cafeaf7631e9d7048b2653e4c9165c179d8ce7
Ricky <rickycxj@gmail.com>
2019-03-28 11:00:51 +0800

    资讯增加热门选项

--- app/admin/info.rb
+++ app/admin/info.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Info do
-  config.sort_order = 'id_desc'
+  config.sort_order = 'position_desc'
 
-  permit_params :title, :image, :description, :source, :published, :created_at
+  permit_params :title, :image, :description, :source, :published, :created_at, :hot
   form partial: 'form'
 
   index do
@@ -10,5 +10,31 @@ ActiveAdmin.register Info do
 
   show do
     render 'show', context: self
+  end
+
+  # 上热门
+  member_action :hot, method: :post do
+    resource.hot!
+    redirect_back fallback_location: admin_infos_url, notice: '上热门成功'
+  end
+
+  # 取消上热门
+  member_action :unhot, method: :post do
+    resource.unhot!
+    redirect_back fallback_location: admin_infos_url, notice: '取消热门成功'
+  end
+
+  member_action :reposition, method: :post do
+    item = Info.find(params[:id])
+    next_item = params[:next_id] && Info.find(params[:next_id].split('_').last)
+    prev_item = params[:prev_id] && Info.find(params[:prev_id].split('_').last)
+    position = if params[:prev_id].blank?
+                 next_item.position + 100000
+               elsif params[:next_id].blank?
+                 prev_item.position / 2
+               else
+                 (prev_item.position + next_item.position) / 2
+               end
+    item.update(position: position)
   end
 end
