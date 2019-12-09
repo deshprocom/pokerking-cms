e3a26b387e8a0d258ef466b79fb56e0fe1fde898
ricky <ricky@rickydeiMac.local>
2019-12-09 20:45:51 +0800

    优化控制面板

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -13,4 +13,18 @@ module ChartsHelper
       User.group_by_week(:created_at, format: '%m-%d', last: 8).count
     end
   end
+
+  def recent_users_login_data
+    Rails.cache.fetch('recent_users_line_data', expires_in: 10.minutes) do
+      User.group_by_day(:updated_at,
+                        range: 2.weeks.ago.midnight..Time.now,
+                        format: '%m-%d').count
+    end
+  end
+
+  def recent_weeks_users_login_data
+    Rails.cache.fetch('recent_weeks_users_column_data', expires_in: 10.minutes) do
+      User.group_by_week(:updated_at, format: '%m-%d', last: 8).count
+    end
+  end
 end
